e1122e2e5df69aba0fe645a0178d40eb4fa123fa
EthanLee <809197443@qq.com>
2024-08-17 18:03:35 +0800

    页面缓存

---
+++ app/src/main/java/com/blues/flutterapp/FlutterApplication.kt
@@ -0,0 +1,24 @@
+package com.blues.flutterapp
+
+import android.app.Application
+import android.util.Log
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class FlutterApplication : Application() {
+
+    companion object {
+        private const val TAG = "FlutterApplication"
+        const val FLUTTER_ENGINE_ID = "flutter_engine_id"
+    }
+
+    override fun onCreate() {
+        Log.d(TAG, "onCreate")
+        super.onCreate()
+        FlutterEngineCache.getInstance()
+            .put(FLUTTER_ENGINE_ID, FlutterEngine(this).apply {
+                dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
+            })
+    }
+}
--- app/src/main/java/com/blues/flutterapp/MainActivity.kt
+++ app/src/main/java/com/blues/flutterapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.blues.flutterapp
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.blues.flutterapp.FlutterApplication.Companion.FLUTTER_ENGINE_ID
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<View>(R.id.main_bt).setOnClickListener {
-            startActivity(Intent(this, FlutterAc::class.java))
+            startActivity(FlutterActivity.withCachedEngine(FLUTTER_ENGINE_ID).build(this))
         }
     }
 }
